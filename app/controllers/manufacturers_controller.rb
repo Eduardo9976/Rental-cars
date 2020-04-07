0732da1d3e607d2e610a1f965e602423a1e74526
Eduardo <eduardo_2407@me.com>
2020-04-06 21:34:32 -0300

    Admin_register_manufactorers

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -5,4 +5,19 @@ class ManufacturersController < ApplicationController
   def show
     @manufacturer = Manufacturer.find(params[:name])
   end  
+
+  def new
+    @manufacturer = Manufacturer.new
+  end
+  
+  def create
+    name = param[:manufacturer] [:name]
+    @manufacturer = Manufacturer.new
+    @manufacturer.name = name
+    if @manufacturer.save
+      redirect_to @manufacturer
+    else
+      render :new
+    end    
+  end   
 end  
